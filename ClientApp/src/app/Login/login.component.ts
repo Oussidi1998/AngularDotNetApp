df1ed38be6e478d4810926d3ece6e855407ede17
Oussidi Mohamed <osidi1998@gmail.com>
2020-06-09 02:42:01 +0100

    Fixing Logout for azure

--- ClientApp/src/app/Login/login.component.ts
+++ ClientApp/src/app/Login/login.component.ts
@@ -24,6 +24,7 @@ export class LoginComponent implements OnInit {
     }
   }
 
+
   ngOnInit() {
     this.formLogin = this.formBuilder.group({
       username: ['Oussidi', Validators.required],
--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,27 +1,8 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { AuthService } from './services/auth.service';
-import { User } from './models/user';
+import { Component} from '@angular/core';
 
 @Component({ selector: 'app', templateUrl: './app.component.html' })
 export class AppComponent {
-  currentUser: User;
 
+  constructor() {}
 
-  constructor(
-    private router: Router,
-    private AuthService: AuthService
-  ) {
-    if (AuthService.getUserToken() !== null) {
-      this.AuthService.getUserData().subscribe(x => this.currentUser = x.value);
-    } else {
-      this.router.navigate(['/login']);
-    }
-  }
-
-  logout() {
-    this.AuthService.logout();
-    this.router.navigateByUrl('/login');
-  }
 }
